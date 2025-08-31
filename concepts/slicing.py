de9b8523ad9933d6ae55dff13dec7b5e69f7e122
HimanshuLadva <himanshuladva2241@gmail.com>
2025-08-31 21:34:57 +0530

    learning slicing concept

---
+++ concepts/slicing.py
@@ -0,0 +1,14 @@
+# slicing
+arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(arr[8:1:-2])
+print(arr[8:2:-1])
+print(arr[-3:])
+print(arr[:-3])
+print(arr[-2:1:-2])
+print(arr[-3:1:-2])
+print(arr[1:1])
+
+nums = [10,20,30,40,50]
+print(nums[-2:0:-2])
+print(nums[1:-1])
+print(list(range(10)))
