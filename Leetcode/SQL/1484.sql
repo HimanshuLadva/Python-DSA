e4fe2f4dafe7c44d148f0486f9c4b63857935d40
HimanshuLadva <himanshuladva2241@gmail.com>
2025-10-04 21:38:21 +0530

    complete today's sql question

---
+++ Leetcode/SQL/1484.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/group-sold-products-by-the-date?envType=problem-list-v2&envId=database
+
+select sell_date,count(*) as num_sold, 
+STRING_AGG(product, ',') WITHIN GROUP (ORDER BY product) AS products 
+from ( SELECT DISTINCT sell_date, product FROM Activities) AS distinct_products
+group by sell_date
