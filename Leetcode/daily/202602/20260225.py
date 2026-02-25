fbfc317f62a0ed913e5e70ead8f5a1aaac0efe98
HimanshuLadva <himanshuladva2241@gmail.com>
2026-02-25 21:26:54 +0530

    complete today's streaks

---
+++ Leetcode/daily/202602/20260225.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits/description/?envType=daily-question&envId=2026-02-25
+
+from typing import List
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        arr.sort(key=lambda x: (x.bit_count(), x))
+        return arr
