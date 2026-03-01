8e14dcacb52f0548846fba9f860527db2b087358
HimanshuLadva <himanshuladva2241@gmail.com>
2026-03-01 22:59:49 +0530

    complete today's streaks

---
+++ Leetcode/daily/202603/20260301.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/?envType=daily-question&envId=2026-03-01
+#howtowork
+class Solution:
+    def minPartitions(n: str) -> int:
+        return int(max(n))
+    
+sol = Solution()
+sol.minPartitions("82734")
