d95f410e448e193805a55b5a9fcb20e34d767458
HimanshuLadva <himanshuladva2241@gmail.com>
2026-03-19 00:04:47 +0530

    complete today's streaks

---
+++ Leetcode/daily/202603/20260318.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/description/?envType=daily-question&envId=2026-03-18
+from typing import List
+class Solution:
+    #howtowork
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        n, m = len(grid), len(grid[0])
+        cols = [0] * m
+        res = 0
+
+        for i in range(n):
+            row_sum = 0
+            for j in range(m):
+                cols[j] += grid[i][j]
+                row_sum += cols[j]
+                if row_sum <= k:
+                    res += 1
+        return res
+    
+sol = Solution()
+print(sol.countSubmatrices(grid = [[7,6,3],[6,6,1]], k = 18))
