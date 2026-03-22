ebec3cb9cabf98617d2800144eff00f1c9fed715
HimanshuLadva <himanshuladva2241@gmail.com>
2026-03-22 12:38:22 +0530

    learn zip() function and complete today's streaks

---
+++ Leetcode/daily/202603/20260322.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/description/?envType=daily-question&envId=2026-03-22
+from typing import List
+class Solution:
+    def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
+        
+        for i in range(4):
+            mat = list(zip(*mat))
+            mat = [list(row[::-1]) for row in mat]
+
+            if mat == target:
+                return True
+
+        return False
+    
+sol = Solution()
+# sol.findRotation(mat = [[0,0,0],[0,1,0],[1,1,1]], target = [[1,1,1],[0,1,0],[0,0,0]])
+sol.findRotation(mat = [[1,2,3],[4,5,6],[7,8,9]], target = [[7,4,1],[8,5,2],[9,6,3]])
--- concepts/Zip.py
+++ concepts/Zip.py
@@ -2,5 +2,6 @@ arr1 = [1,2,3]
 arr2 = [1,2,'hello']
 arr3 = [1,2,3]
 
+#defination = zip() pair elements index by index of multiple list
 for a,b,c in zip(arr1, arr2, arr3):
     print(f"a = {a} b = {b} c = {c}")
