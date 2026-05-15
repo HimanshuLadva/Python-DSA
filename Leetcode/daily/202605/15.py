8398fff70def60c70a43f4e42a0d3ecc9a6e4976
HimanshuLadva <himanshuladva2241@gmail.com>
2026-05-15 07:12:38 +0530

    complete today's streaks

---
+++ Leetcode/daily/202605/15.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/?envType=daily-question&envId=2026-05-15
+from typing import List
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        return min(nums)
+        
