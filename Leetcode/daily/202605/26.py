48ae648ae9a452d410f7b229c84fd20fcbb13e1d
HimanshuLadva <himanshuladva2241@gmail.com>
2026-05-26 06:05:48 +0530

    complete today's streaks

---
+++ Leetcode/daily/202605/26.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfSpecialChars(self, word: str) -> int:
+        upper_word = {x.lower() for x in word if x.isupper()}
+        lower_word = {x for x in word if x.islower()}
+
+        return len(upper_word & lower_word)
+    
+    # 3ms
+    def numberOfSpecialChars(self, word: str) -> int:
+        words = set([x for x in word if x.isupper()])
+        # print(words) 
+        
+        count = 0
+        for x in words:
+            if x.lower() in word:
+                count += 1
+        return count
+
+sol = Solution()
+print(sol.numberOfSpecialChars(word = "aaAbcBC"))
+print(sol.numberOfSpecialChars(word = "CCc"))
