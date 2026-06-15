2a8756cd694ae65c2ba76a3a3e29bb6c9bd26edb
HimanshuLadva <himanshuladva2241@gmail.com>
2026-06-15 21:02:43 +0530

    complete today's streaks

--- Leetcode/daily/202606/14.py
+++ Leetcode/daily/202606/14.py
@@ -1,51 +1,33 @@
-# https://leetcode.com/problems/weighted-word-mapping/description/?envType=daily-question&envId=2026-06-13
+# https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/?envType=daily-question&envId=2026-06-14
 
-from typing import List
-from collections import defaultdict
-import string
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
-    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        master = {
-            0: 'z',
-            1: 'y',
-            2: 'x',
-            3: 'w',
-            4: 'v',
-            5: 'u',
-            6: 't',
-            7: 's',
-            8: 'r',
-            9: 'q',
-            10: 'p',
-            11: 'o',
-            12: 'n',
-            13: 'm',
-            14: 'l',
-            15: 'k',
-            16: 'j',
-            17: 'i',
-            18: 'h',
-            19: 'g',
-            20: 'f',
-            21: 'e',
-            22: 'd',
-            23: 'c',
-            24: 'b',
-            25: 'a'
-        }
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        curr = head
+        arr = []
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
 
-        frm_weight = defaultdict(str)
-        for letter, weight in zip(string.ascii_lowercase, weights):
-            frm_weight[letter] = weight
+        res = []
+        i = 0
+        j = len(arr) - 1
 
-        ans = ""
-        for word in words:
-            word_sum = 0
-            for ch in word:
-                word_sum += frm_weight[ch]
-            ans += master[word_sum % 26]
-
-        return ans
+        while i < j:
+            res.append(arr[i] + arr[j])
+            i += 1
+            j -= 1
+        
+        # print(res)
+        return max(res)
     
 sol = Solution()
-print(sol.mapWordWeights(words = ["abcd","def","xyz"], weights = [5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2]))
+head = ListNode(5)
+head.next = ListNode(4)
+head.next.next = ListNode(2)
+head.next.next.next = ListNode(1)
+sol.pairSum(head)
--- Leetcode/daily/202606/15.py
+++ Leetcode/daily/202606/15.py
@@ -1,33 +1,46 @@
-# https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/?envType=daily-question&envId=2026-06-14
+# https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/?envType=daily-question&envId=2026-06-15
 
+# Definition for singly-linked list.
 from typing import Optional
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 class Solution:
-    def pairSum(self, head: Optional[ListNode]) -> int:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        n = 0
         curr = head
-        arr = []
         while curr:
-            arr.append(curr.val)
+            n += 1
             curr = curr.next
-
-        res = []
-        i = 0
-        j = len(arr) - 1
-
-        while i < j:
-            res.append(arr[i] + arr[j])
-            i += 1
-            j -= 1
         
-        # print(res)
-        return max(res)
+        mid = n // 2
+        curr = head
+        prev = None
+        i = 0
+        while i < mid:
+            prev = curr
+            curr = curr.next
+            i += 1
+
+        if prev:
+            prev.next = curr.next
+        else:
+            head = None
+
+        # curr = head
+        # while curr:
+        #     print(curr.val)
+        #     curr = curr.next
+        return head
     
 sol = Solution()
-head = ListNode(5)
-head.next = ListNode(4)
-head.next.next = ListNode(2)
-head.next.next.next = ListNode(1)
-sol.pairSum(head)
+head = ListNode(1)
+head.next = ListNode(3)
+head.next.next = ListNode(4)
+head.next.next.next = ListNode(7)
+head.next.next.next.next = ListNode(1)
+head.next.next.next.next.next = ListNode(2)
+head.next.next.next.next.next.next = ListNode(6)
+sol.deleteMiddle(head)
+    
