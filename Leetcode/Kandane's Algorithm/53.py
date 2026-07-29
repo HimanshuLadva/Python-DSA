64bb29d24fc23ee850fd89032074ec3b27b5261e
HimanshuLadva <himanshuladva2241@gmail.com>
2026-07-29 08:02:06 +0530

    complete kandane's 53

---
+++ Leetcode/Kandane's Algorithm/53.py
@@ -0,0 +1,16 @@
+from typing import List
+from math import inf
+class Solution:
+    def maxSubArrayV1(self, nums: List[int]) -> int:
+        curr_sum = -inf
+        max_sum = -inf
+
+        for num in nums:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+
+        # print(max_sum)
+        return max_sum
+
+sol = Solution()
+sol.maxSubArray(nums = [-2,1,-3,4,-1,2,1,-5,4])
